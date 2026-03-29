4f98d1cf1d8655605d7b00edb8785b7767a2bfaf
mike <vatolkinmihajlo@gmail.com>
2026-03-29 15:14:13 +0300

    + 2 env files, new dockerfile tested

--- task_manager/src/infrastructure/pool.js
+++ task_manager/src/infrastructure/pool.js
@@ -1,12 +1,19 @@
 // db.js
 import {Pool} from 'pg'
+import dotenv from 'dotenv';
+dotenv.config({path: "../../.env_app"});
 
 const pool = new Pool({
-    user: 'user2',
-    host: 'localhost',
-    database: 'mydb',
-    password: 'pass',
-    port: 5433,
+    // eslint-disable-next-line no-undef
+    host: process.env.DB_HOST,
+    // eslint-disable-next-line no-undef
+    port: process.env.DB_PORT,
+    // eslint-disable-next-line no-undef
+    user: process.env.DB_USER,
+    // eslint-disable-next-line no-undef
+    password: process.env.DB_PASSWORD,
+    // eslint-disable-next-line no-undef
+    database: process.env.DB_NAME,
 });
 
 export default pool;
