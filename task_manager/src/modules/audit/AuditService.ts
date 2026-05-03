1f8bb6344af817f805cfe634551819b76adcf078
MykhailoVat <vatolkinmihajlo@gmail.com>
2026-05-03 03:53:40 +0300

    made log prettier

--- task_manager/src/modules/audit/AuditService.ts
+++ task_manager/src/modules/audit/AuditService.ts
@@ -4,13 +4,13 @@ import fs from "fs";
 
 class AuditService implements IAuditService {
     log(event: TAuditEvent): void {
-        const entry = JSON.stringify(event) + "\n"
+        const entry = `[${event.occurredAt}] ${event.operation} | id=${event.entityId} | ${JSON.stringify(event.payload)}\n`;
 
         fs.mkdir(AUDIT_CONFIG.logDir, { recursive: true } , (err) => {
             if (err) throw err;
         });
 
-        fs.appendFile(AUDIT_CONFIG.logFilePath, JSON.stringify(entry), 'utf8', err => {
+        fs.appendFile(AUDIT_CONFIG.logFilePath, entry, 'utf8', err => {
             if (err) throw err;
         });
     }
