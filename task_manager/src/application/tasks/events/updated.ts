f5cc8d1f04814a3163b6e7c116348feba7dfa278
Impe11e <1mpellee@gmail.com>
2026-05-05 20:19:42 +0300

    feat: add synchronous and asynchronous communication between components

---
+++ task_manager/src/application/tasks/events/updated.ts
@@ -0,0 +1,23 @@
+import type { TaskEntity } from '../../../domain/tasks/entities/taskEntity';
+
+class TaskUpdatedEvent {
+    public readonly operation: string;
+    public readonly entityId: number;
+    public readonly payload: any;
+    public readonly occurredAt: Date;
+
+    constructor(task: TaskEntity) {
+        this.operation = "Task Updated";
+        this.entityId = task.id!;
+        this.payload = {
+            title: task.title,
+            description: task.description,
+            status: task.status,
+            priority: task.priority,
+            dueDate: task.dueDate
+        };
+        this.occurredAt = new Date();
+    }
+}
+
+export default TaskUpdatedEvent;
